62cabae6cfd610f0e5bf92bf6a2b496a4e5f874f
Scott Frees <scott.frees@gmail.com>
2014-10-19 20:10:10 -0400

    Jobs searchable by complete (true/false) and type (long string)

--- serve/routes/jobs.js
+++ serve/routes/jobs.js
@@ -3,11 +3,15 @@ var db = require("../db");
 
 exports.list = function (req, res) {
   var owner = req.query.owner;
+  var type = req.query.type;
+  var complete = req.query.complete;
   var q = {};
-  if ( owner) {
-      q.owner = owner;
-  }
-  console.log(q);
+
+  if ( owner) q.owner = owner;
+  if ( type ) q.type = type;
+  if ( complete) q.complete = complete;
+
+  console.log(JSON.stringify(q));
   db.jobs.find(q, function(err, result){
       if ( err ) {
           res.status(404).end('Job listing could not found');
