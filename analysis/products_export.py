ce82359ae4a7e15fa3fd628c146c40c5f5d00899
Scott Frees <scott.frees@gmail.com>
2014-11-05 18:20:25 -0500

    multiple product test/export

---
+++ analysis/products_export.py
@@ -0,0 +1,14 @@
+
+from pymongo import MongoClient
+client = MongoClient()
+db = client['chrome']
+collect = db['mrna']
+
+results = db.mrna.aggregate(
+ [{'$match': {'organism':'Homo sapiens'}},
+  {'$group': { '_id':'$gene_id', 'products' : { '$sum':1}, 'transcript' : {'$push' : '$accession'}, 'cds' : {'$push':'$cds'}}},
+  {'$match' :{'products': {'$gt': 1}}}]);
+
+for result in results['result']:
+  for cds in result['cds']:
+    print (result['_id'], '\t', result['products'], "\t", cds['start'], '\t', cds['end'])
