b1fa5bf587494b48878e7b8e03011b3500cba73f
mliu2 <mliu2@ramapo.edu>
2015-10-14 16:13:59 -0400

    PolyA filter for UTR3 and 25bp

--- seed/seed_polya.py
+++ seed/seed_polya.py
@@ -51,46 +51,47 @@ with open(local_path) as f:
         acc = get_chrom_accession(ch)
         start_pos = line.split()[1]
         end_pos = line.split()[2]
+        if line.split()[6] == "UTR3" and abs(int(end_pos)-int(start_pos)) <= 25 : 
 
-        url = seq_url + '/chrom/locusmap/' +acc + "/" + start_pos
-        response = requests.get(url)
-        if response.status_code == requests.codes.ok :
-            data = response.json()
-            status = "NOT FOUND"
-            final_pos = start_pos
-            if len(data['mrna']) > 0:
-                status = "INTRON"
-                for mrna in data['mrna']:
-                    if mrna['locus'] >= 0:
-                        status = "MAPPED TO " + str(mrna['locus'])
-                        with_mrna += 1
-                    else:
-                        url = seq_url + '/chrom/locusmap/' +acc + "/" + end_pos
-                        response = requests.get(url)
-                        if response.status_code == requests.codes.ok :
-                            data = response.json()
-                            status = "NOT FOUND"
-                            final_pos = end_pos
-                            if len(data['mrna']) > 0:
-                                status = "INTRON"
-                                for mrna in data['mrna']:
-                                    if mrna['locus'] >= 0:
-                                        status = "MAPPED TO " + str(mrna['locus'])
-                                        with_mrna += 1
-            else:
-                url = seq_url + '/chrom/locusmap/' +acc + "/" + end_pos
-                response = requests.get(url)
-                if response.status_code == requests.codes.ok :
-                    data = response.json()
-                    status = "NOT FOUND"
-                    final_pos = end_pos
-                    if len(data['mrna']) > 0:
-                        status = "INTRON"
-                        for mrna in data['mrna']:
-                            if mrna['locus'] >= 0:
-                                status = "MAPPED TO " + str(mrna['locus'])
-                                with_mrna += 1
-            processed += 1
+            url = seq_url + '/chrom/locusmap/' +acc + "/" + start_pos
+            response = requests.get(url)
+            if response.status_code == requests.codes.ok :
+                data = response.json()
+                status = "NOT FOUND"
+                final_pos = start_pos
+                if len(data['mrna']) > 0:
+                    status = "INTRON"
+                    for mrna in data['mrna']:
+                        if mrna['locus'] >= 0:
+                            status = "MAPPED TO " + str(mrna['locus'])
+                            with_mrna += 1
+                        else:
+                            url = seq_url + '/chrom/locusmap/' +acc + "/" + end_pos
+                            response = requests.get(url)
+                            if response.status_code == requests.codes.ok :
+                                data = response.json()
+                                status = "NOT FOUND"
+                                final_pos = end_pos
+                                if len(data['mrna']) > 0:
+                                    status = "INTRON"
+                                    for mrna in data['mrna']:
+                                        if mrna['locus'] >= 0:
+                                            status = "MAPPED TO " + str(mrna['locus'])
+                                            with_mrna += 1
+                else:
+                    url = seq_url + '/chrom/locusmap/' +acc + "/" + end_pos
+                    response = requests.get(url)
+                    if response.status_code == requests.codes.ok :
+                        data = response.json()
+                        status = "NOT FOUND"
+                        final_pos = end_pos
+                        if len(data['mrna']) > 0:
+                            status = "INTRON"
+                            for mrna in data['mrna']:
+                                if mrna['locus'] >= 0:
+                                    status = "MAPPED TO " + str(mrna['locus'])
+                                    with_mrna += 1
+                processed += 1
 
-            # MAKE THE POLYA record and insert it into the collection
-            print (with_mrna , " / " , processed, "   -  Accession ", acc, "Locus = ", final_pos, " - ", status)
+                # MAKE THE POLYA record and insert it into the collection
+                print (with_mrna , " / " , processed, "   -  Accession ", acc, "Locus = ", final_pos, " - ", status)
