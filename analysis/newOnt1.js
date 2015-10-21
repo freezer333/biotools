2a8a62a2b2f91c37ec29663cbe3ccef755048866
mmayberr <Melissa@The-Cheshire-Cat-3.local>
2015-10-21 15:11:49 -0400

    Initial Commit

--- analysis/newOnt1.js
+++ analysis/newOnt1.js
@@ -3,7 +3,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 mongoose.connect('mongodb://localhost:27017/chrome');
 
+/*
 var fs = require('graceful-fs'); // file system with request queue
+*/
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -13,6 +15,9 @@ db.once('open', function (ref) {
         MrnaSchema = new Schema({ 'organism': String, 'accession': String, 'g4s': Array},
                    { collection : 'mrna' });
         var Mrna = mongoose.model('Mrna', MrnaSchema);
+        
+        AnalysisSchema = new Schema({ '_id': Schema.Types.ObjectId, 'gscore_min': Number, 'g4location': Number, 'validMRNA': Array, 'invalidMRNA': Array, });
+        var Analysis = mongoose.model('Analysis', AnalysisSchema);
         
         Mrna.
         find({
@@ -28,13 +33,17 @@ db.once('open', function (ref) {
              var minOntnum;
              console.log("What is the minimum gscore and rna location?\n Use 5 for 5'-UTR, 3 for 3'-UTR, or 1 for CDS");
              prompt.get(['mingscore','rnaloc'], function (err, result) {
-             
+             /*
              fs.writeFile("./validg4.txt", "", function(err){
                           if(err) { return console.log(err); }
                           });
              fs.writeFile("./invalidg4.txt", "", function(err){
                           if(err) { return console.log(err); }
                           });
+              */
+                        var validList =[];
+                        var invalidList=[];
+                        
              for( rna in mrnaList ){
                 var isg4 = false; // true if valid g4 is found
                 var thisrna = mrnaList[rna];
@@ -54,17 +63,24 @@ db.once('open', function (ref) {
             
                 }
                 if(isg4){
-                    fs.appendFile("./validg4.txt", thisrna["accession"] + "\n", function(err) {
+                    /*fs.appendFile("./validg4.txt", thisrna["accession"] + "\n", function(err) {
                            if(err) { return console.log(err); }
+                     
                            });
+                     */
+                        validList.push(thisrna["accession"]);
+                        
                 }else{
-                    fs.appendFile("./invalidg4.txt", thisrna["accession"] + "\n", function(err) {
+                    /*fs.appendFile("./invalidg4.txt", thisrna["accession"] + "\n", function(err) {
                            if(err) { return console.log(err); }
                            });
+                     */
+                        invalidList.push(thisrna["accession"]);
                 }
-             }
-                        };
-                        });
+                        }
+                        }
+                        Analysis.create({ '_id': Schema.Types.ObjectId, 'gscore_min': mingscore, 'g4location': rnaloc, 'validMRNA': validList, 'invalidMRNA': invalidList});
+                    });
              });
         });
 // mongoose.connection.close(); // MAKE THIS WAIT FOR PREVIOUS CODE
