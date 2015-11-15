747ff27f1be86f14f20af3399edc52e62fac8736
mliu2 <mliu2@ramapo.edu>
2015-11-15 14:41:08 -0500

    Changed polya seeding to work with taxon_ids like the other seed files. Added apadb_seed entry to mouse and human seed files so the polya seeder can find the file. The two seed files are stored locally in seed/apadb_seeds

--- seed/seed_polya.py
+++ seed/seed_polya.py
@@ -26,11 +26,10 @@ client = MongoClient()
 db = client.chrome
 #------------------------------------------------------------
 
-# only working with human
-with open('seeds/9606.json') as json_file:
-    seed = json.load(json_file)
 
-def get_chrom_accession(common):
+
+
+def get_chrom_accession(common, seed):
 
     for record in seed['chromosomes']:
         v = "chr" + record['common_id']
@@ -78,72 +77,49 @@ def get_urich_motifs(seq):
     return motifs
     
 
+# check taxon id
+if len(sys.argv) > 1 :
+    taxon_ids = sys.argv[1:]
+else :
+    print("You must specify taxon id's of the organism you want to build.")
+    print("For example:  '",sys.argv[0]," 9606' would install Homo sapiens")
+    print("Currently only supporting 9606 (Homo sapiens) and 10090 (Mus musculus)")
+    sys.exit(0);
+for taxon_id in sorted(taxon_ids) :
+    with open("seeds/"+ taxon_id + ".json") as json_file:
+        seeds = json.load(json_file)
 
 
-config = configparser.ConfigParser()
-config.read('seed_sources.ini')
-
-seq_url = config['chrom']['serve_url']
-url = config['polyA']['base_url'] + config['polyA']['download_filename']
-local_path = config['local']['download_dir'] + "/" + config['polyA']['download_filename'];
-
-
-print("Now processing APADB - make sure your webapp is running and you have the polyA bed file in temp!")
-
-processed = 0
-with_mrna = 0
-with open(local_path) as f:
-    for line in f:
-        ch = line.split()[0]
-        acc = get_chrom_accession(ch)
-        start_pos = line.split()[1]
-        end_pos = line.split()[2]
-        status="NOT FOUND"
-        url =""
-        region= line.split()[6]
-        if (region == "UTR3" or region =="Extension") and abs(int(end_pos)-int(start_pos)) <= 25: 
-                url = seq_url + '/chrom/locusmap/' +acc + "/" + start_pos
-                response = requests.get(url)
-                if response.status_code == requests.codes.ok :
-                    data = response.json()
-                    found=0
-                    mapped_mrna = []
-                    #print(data)
-                    final_pos = start_pos
-                    if len(data['mrna']) > 0:
-                        status = "INTRON"
-                        for mrna in data['mrna']:
-                            if mrna['locus'] >= 0:
-                                status = "MAPPED TO " + str(mrna['locus'])
-                                with_mrna += 1
-                                mapped_mrna.append(mrna['accession'])
-                                
-
-                            #check the end position   
-                            else:
-                                url = seq_url + '/chrom/locusmap/' +acc + "/" + end_pos
-                                response = requests.get(url)
-                                if response.status_code == requests.codes.ok :
-                                    data = response.json()
-                                    status = "NOT FOUND"
-                                    final_pos = end_pos
-                                    if len(data['mrna']) > 0:
-                                        status = "INTRON"
-                                        for mrna in data['mrna']:
-                                            if mrna['locus'] >= 0:
-                                                status = "MAPPED TO " + str(mrna['locus'])
-                                                with_mrna += 1
-                                                mapped_mrna.append(mrna['accession'])
-                                     
-                                 
-                                            
-                else:
-                    url = seq_url + '/chrom/locusmap/' +acc + "/" + end_pos
+    config = configparser.ConfigParser()
+    config.read('seed_sources.ini')
+    
+    seq_url = config['chrom']['serve_url']
+    #url = config['polyA']['base_url'] + config['polyA']['download_filename']
+    local_path = config['local']['download_dir'] + "/" + seeds["apadb_seed"];
+    
+    
+    print("Now processing APADB - make sure your webapp is running and you have the polyA bed file in temp!")
+    
+    processed = 0
+    with_mrna = 0
+    with open(local_path) as f:
+        for line in f:
+            ch = line.split()[0]
+            acc = get_chrom_accession(ch, seeds)
+            start_pos = line.split()[1]
+            end_pos = line.split()[2]
+            status="NOT FOUND"
+            url =""
+            region= line.split()[6]
+            if (region == "UTR3" or region =="Extension") and abs(int(end_pos)-int(start_pos)) <= 25: 
+                    url = seq_url + '/chrom/locusmap/' +acc + "/" + start_pos
                     response = requests.get(url)
                     if response.status_code == requests.codes.ok :
                         data = response.json()
-                        status = "NOT FOUND"
-                        final_pos = end_pos
+                        found=0
+                        mapped_mrna = []
+                        #print(data)
+                        final_pos = start_pos
                         if len(data['mrna']) > 0:
                             status = "INTRON"
                             for mrna in data['mrna']:
@@ -151,46 +127,80 @@ with open(local_path) as f:
                                     status = "MAPPED TO " + str(mrna['locus'])
                                     with_mrna += 1
                                     mapped_mrna.append(mrna['accession'])
-
-                processed += 1
-
-                # MAKE THE POLYA record and insert it into the collection
-                print (with_mrna , " / " , processed, "   -  Accession ", acc, "Locus = ", final_pos, " - ", status)
-
-                                 
-        if (status.split()[0] == "MAPPED"):
-            #extract gene id from data
-            genes=data['genes']
-            gene_id=""
-            for g in genes:
-                gene_id =g['gene_id'] 
-                    #make sure mrna accessions are distinct
-            mrna_set= set(mapped_mrna)
-            seq = get_Seq(start_pos,end_pos,acc)
-            us= get_urich_motifs(seq)
-                #the find g4 has lots of extra info, make less cluttered
-            g4= find(seq)
-            g_quad = []
-            for g in g4:
-                tmp_g = dict()
-                tmp_g['gscore']= g['gscore']
-                tmp_g['tetrads']= g['tetrads']
-                tmp_g['length']= g['length']
-                tmp_g['sequence']= g['sequence']
-                tmp_g['start']= g['start']
-                g_quad.append(tmp_g)
-
-                   
+                                    
     
-            record = {
-                "gene_id": gene_id,
-                "start":start_pos,
-                "end":end_pos,
-                "mrna": list(mrna_set),
-                "URS": us,
-                "g_quad":g_quad,
-                "region":region
-            }
-            db.polyA.insert_one(record)
+                                #check the end position   
+                                else:
+                                    url = seq_url + '/chrom/locusmap/' +acc + "/" + end_pos
+                                    response = requests.get(url)
+                                    if response.status_code == requests.codes.ok :
+                                        data = response.json()
+                                        status = "NOT FOUND"
+                                        final_pos = end_pos
+                                        if len(data['mrna']) > 0:
+                                            status = "INTRON"
+                                            for mrna in data['mrna']:
+                                                if mrna['locus'] >= 0:
+                                                    status = "MAPPED TO " + str(mrna['locus'])
+                                                    with_mrna += 1
+                                                    mapped_mrna.append(mrna['accession'])
+                                         
+                                     
+                                                
+                    else:
+                        url = seq_url + '/chrom/locusmap/' +acc + "/" + end_pos
+                        response = requests.get(url)
+                        if response.status_code == requests.codes.ok :
+                            data = response.json()
+                            status = "NOT FOUND"
+                            final_pos = end_pos
+                            if len(data['mrna']) > 0:
+                                status = "INTRON"
+                                for mrna in data['mrna']:
+                                    if mrna['locus'] >= 0:
+                                        status = "MAPPED TO " + str(mrna['locus'])
+                                        with_mrna += 1
+                                        mapped_mrna.append(mrna['accession'])
+    
+                    processed += 1
+    
+                    # MAKE THE POLYA record and insert it into the collection
+                    print (with_mrna , " / " , processed, "   -  Accession ", acc, "Locus = ", final_pos, " - ", status)
+    
+                                     
+            if (status.split()[0] == "MAPPED"):
+                #extract gene id from data
+                genes=data['genes']
+                gene_id=""
+                for g in genes:
+                    gene_id =g['gene_id'] 
+                        #make sure mrna accessions are distinct
+                mrna_set= set(mapped_mrna)
+                seq = get_Seq(start_pos,end_pos,acc)
+                us= get_urich_motifs(seq)
+                    #the find g4 has lots of extra info, make less cluttered
+                g4= find(seq)
+                g_quad = []
+                for g in g4:
+                    tmp_g = dict()
+                    tmp_g['gscore']= g['gscore']
+                    tmp_g['tetrads']= g['tetrads']
+                    tmp_g['length']= g['length']
+                    tmp_g['sequence']= g['sequence']
+                    tmp_g['start']= g['start']
+                    g_quad.append(tmp_g)
+    
+                       
+        
+                record = {
+                    "gene_id": gene_id,
+                    "start":start_pos,
+                    "end":end_pos,
+                    "mrna": list(mrna_set),
+                    "URS": us,
+                    "g_quad":g_quad,
+                    "region":region
+                }
+                db.polyA.insert_one(record)
 
                
