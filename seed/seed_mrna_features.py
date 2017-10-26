d5351ce4b40e54add40ef86ea376d21fb33ac1e0
Scott Frees <scott.frees@gmail.com>
2017-10-26 07:50:13 -0400

    build 37 revert

--- seed/seed_mrna_features.py
+++ seed/seed_mrna_features.py
@@ -1,7 +1,7 @@
- # This program will add mRNA sequence features to mRNA listings into a MongoDb.
- # Note - the mRNA listings must already be present - if the mRNA referred to
- # in the sequence file read does not already exist in the mRNA collection, it
- # is ignored.
+# This program will add mRNA sequence features to mRNA listings into a MongoDb.
+# Note - the mRNA listings must already be present - if the mRNA referred to
+# in the sequence file read does not already exist in the mRNA collection, it
+# is ignored.
 import urllib.request
 import shutil
 import os
@@ -34,16 +34,17 @@ collect = db.mrna
 #------------------------------------------------------------
 
 
-print ("++++++++++++++++++++++++++++++++++++++++++++++")
-print ("mRNA Sequence Features (Homo sapien) -> MongoDb")
-print ("----------------------------------------------")
+print("++++++++++++++++++++++++++++++++++++++++++++++")
+print("mRNA Sequence Features (Homo sapien) -> MongoDb")
+print("----------------------------------------------")
 
 i = 0
 count = 0
 all_count = 0
-for url in urls :
+for url in urls:
     print("PROCESSING " + url)
-    seq_features_file = root + config['human features']['download_filename'] + str(i)
+    seq_features_file = root + config['human features']['download_filename']
+    seq_features_file = seq_features_file + str(i)
     if not os.path.isfile(seq_features_file):
         print("\t+ Downloading source file from:  ", url)
         with urllib.request.urlopen(url) as response, open(seq_features_file, 'wb') as out_file:
@@ -51,28 +52,27 @@ for url in urls :
 
     seq_features_file = gzip.open(seq_features_file, 'rb')
 
-
-    
     features = dict()
-    #with open(seq_features_file) as f:
+    # with open(seq_features_file) as f:
     for data in seq_features_file:
         line = data.decode('utf-8')
         fields = re.split('\s+', line)
         fields = [x.strip() for x in fields]
         if fields[0] == 'LOCUS':
-            if len(features ) > 0:
+            if len(features) > 0:
                 # save features
                 all_count += 1
                 up = dict()
 
                 if collect.find_and_modify(
-                          {'accession' : features['accession']},
-                          {'$set': features}) != None :
-                    count+= 1
-                print ('Saved ', count, ' / ' , all_count, ' mrna features')
+                    {'accession': features['accession']},
+                        {'$set': features}) != None:
+                    count += 1
+                print('Saved ', count, ' / ', all_count,
+                      ' mrna features - ', url)
                 features = dict()
 
-            features['length'] = fields[2];
+            features['length'] = fields[2]
         if fields[0] == 'DEFINITION':
             features['definition'] = ' '.join(fields[1:])
         if fields[0] == 'VERSION':
@@ -80,12 +80,12 @@ for url in urls :
         if len(fields) >= 2 and fields[1] and fields[1].startswith('/gene='):
             features['gene_name'] = fields[1].split('=')[1].replace('\"', "")
         if fields[1] == 'CDS':
-            pts = fields[2].split("..");
-            if len (pts)  > 1:
+            pts = fields[2].split("..")
+            if len(pts) > 1:
                 start = pts[0].strip()
                 end = pts[1].strip()
-                features['cds'] = { 'start' : start, 'end' : end}
-                features['utr_5'] = { 'start' : '1', 'end' : start}
-                features['utr_3'] = { 'start' : end, 'end' : features['length']}
+                features['cds'] = {'start': start, 'end': end}
+                features['utr_5'] = {'start': '1', 'end': start}
+                features['utr_3'] = {'start': end, 'end': features['length']}
 
     seq_features_file.close()
